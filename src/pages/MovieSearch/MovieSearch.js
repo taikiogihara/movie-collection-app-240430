32c61456360595b6437d06aa43ed71136e5eb2f4
Taiki Ogihara <taikiogihara@Taikis-MacBook-Air.local>
2024-05-02 18:08:22 +0900

    UI updated 4

--- src/pages/MovieSearch/MovieSearch.js
+++ src/pages/MovieSearch/MovieSearch.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Modal from "react-modal";
 import "./MovieSearch.css";
